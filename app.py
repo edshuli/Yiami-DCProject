d0570aaf226f8f9360429e619b3775dcb049889e
Edna <edna_shuli@hotmail.gr>
2020-04-07 00:42:17 +0000

    Add new files css, img

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipe_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://tokyo_ghoul:edna@myfirstcluster-uvyys.mongodb.net/recipe_manager?retryWrites=true&w=majority'
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_posts')
+def get_posts():
+    return render_template("main.html", posts=mongo.db.posts.find())
 
 
 if __name__ == '__main__':
