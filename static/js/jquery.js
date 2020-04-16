4c1988720cee97abfb1151761451d6817778cec2
Edna <edna_shuli@hotmail.gr>
2020-04-16 08:50:39 +0000

    Added new background

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
- 
+    var myNav = $(".navbar");
+
+    $(window).on('scroll', function() {
+    "use strict";
+    if ($(window).scrollTop() >= 100) {
+        myNav.addClass("scroll");
+    }
+    else {
+        myNav.removeClass("scroll");
+    }
+    });
+
+
 
 
 });
